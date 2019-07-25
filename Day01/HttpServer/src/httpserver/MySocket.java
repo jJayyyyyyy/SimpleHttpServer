21cf0ed8f2fd0fffca27cf2069b83faa666c0213
Steve <ljjdbd123@hotmail.com>
2019-07-25 13:16:30 +0000

    update MySocket.java of Day01

--- Day01/HttpServer/src/httpserver/MySocket.java
+++ Day01/HttpServer/src/httpserver/MySocket.java
@@ -1,7 +1,6 @@
 package httpserver;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -9,94 +8,155 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.Charset;
 
-public class MySocket{
-	private String host = "127.0.0.1";
-	private int port = 8888;
-	private Socket socket;
-	
+public class MySocket {
+	private final String LOCALHOST = "127.0.0.1";
+	private final int DEFAULT_PORT = 8888;
+
+	private InetAddress mHost;
+	private int mPort;
+	private Socket mSocket;
+
 	public MySocket() {
 		try {
-			this.socket = new Socket(InetAddress.getByName(host), port);
-			socket.setReuseAddress(true);
-		} catch (Exception e) {
+			mHost = InetAddress.getByName(LOCALHOST);
+			mPort = DEFAULT_PORT;
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void close() {
+	public void send(String msg) {
 		try {
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		MySocket testSocket = new MySocket();
-
-		testSocket.test1();
-//		testSocket.test2();
-//		testSocket.test3();
-
-		testSocket.close();
-	}
-
-	public void test1() {
-		System.out.println("start test1---");
-		try {
-			String msg = "hello world";
-			byte[] txbuf = msg.getBytes(Charset.forName("UTF-8"));
+			// 创建一个 Socket 实例
+			mSocket = new Socket(mHost, mPort);
+			// 开启快速重用
+			mSocket.setReuseAddress(true);
 			
-			OutputStream os = socket.getOutputStream();	// 获取输出流
+			byte[] txbuf = msg.getBytes(Charset.forName("UTF-8"));
+			// 获取输出流
+			OutputStream os = mSocket.getOutputStream();
 			os.write(txbuf);
 			os.close();
-			
-		} catch (Exception e) {
+			// 关闭  socket
+			mSocket.close();
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("end test1---");
 	}
 	
-	public void test2() {
-		System.out.println("start test2---");
+	public void recv() {
 		try {
-			byte[] rxbuf = new byte[16];	// 构造一个字节数组作为接收区，大小是 16 字节，未填充的元素值默认为 0
-			
-			InputStream is = socket.getInputStream();	// 获取输入流
-			is.read(rxbuf);	// 写入接收区
+			// 创建一个 Socket 实例
+			mSocket = new Socket(mHost, mPort);
+			// 开启快速重用
+			mSocket.setReuseAddress(true);
+						
+			// 构造一个字节数组作为接收区, 大小是 16 字节, 未填充的元素值默认初始化为 0
+			byte[] rxbuf = new byte[16];
+			// 获取输入流
+			InputStream is = mSocket.getInputStream();
+			// 写入接收区
+			is.read(rxbuf);
 			is.close();
+			mSocket.close();
 			
-			String msg = new String(rxbuf, Charset.forName("UTF-8"));	// 转换成字符串
+			// 转换成字符串
+			String msg = new String(rxbuf, Charset.forName("UTF-8"));
 			System.out.println(msg);
-		}catch (Exception e) {
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("end test2---");
 	}
 	
-	public void test3() {
-		System.out.println("start test3---");
+	public void recvLineByLine() {
 		try {
-			InputStream is = socket.getInputStream();
-			InputStreamReader isReader = new InputStreamReader(is, Charset.forName("UTF-8"));	// 二进制流转变成文本流
-			BufferedReader bufReader = new BufferedReader(isReader);	// 缓冲区
+			// 创建一个 Socket 实例
+			mSocket = new Socket(mHost, mPort);
+			// 开启快速重用
+			mSocket.setReuseAddress(true);
 			
-			String line = "";
-			while( true ) {
-				line = bufReader.readLine(); // readLine会自动去掉\r\n, 相当于rstrip("\r\n")
-				if( line.equals("q") == true ) {
-					// 注意在判断字符串内容是否相同的时候要用 .equals() 方法
+			// 获取输入流
+			InputStream is = mSocket.getInputStream();
+			// 二进制流转变成文本流
+			InputStreamReader isReader = new InputStreamReader(is, Charset.forName("UTF-8"));
+			// 缓冲区
+			BufferedReader bufReader = new BufferedReader(isReader);
+			
+			while(true) {
+				String line = bufReader.readLine();
+				// readLine会自动去掉\r\n, 相当于rstrip("\r\n")
+				if (line != null) {
+					if(line.equals("q")) {
+						// 注意在判断字符串内容是否相同的时候要用 .equals() 方法
+						System.out.println("quit");
+						break;
+					}
+					else {
+						System.out.println(line);
+					}
+				}
+				else {
 					break;
-				}else {
-					System.out.println(line);
 				}
 			}
+			
 			bufReader.close();
 			isReader.close();
 			is.close();
-		}catch (Exception e) {
+			mSocket.close();
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("end test3---");
+	}
+
+	public static void main(String[] args) {
+		MySocket mySocket = new MySocket();
+		mySocket.send("Hello world!");
+		mySocket.recv();
+		mySocket.recvLineByLine();
 	}
 }
+
+/*
+# https://docs.python.org/3/library/socket.html#example
+import socket
+
+def mySocketTest(option):
+	server_host = '127.0.0.1'
+	port = 8888
+
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((server_host, port))
+		s.listen(1)
+		conn, addr = s.accept()
+
+		if option == 1:
+			print('1: 测试 send()')
+			with conn:
+				data = conn.recv(1024)
+				print(data.decode('utf-8'))
+			print('ok\n')
+
+		elif option == 2:
+			print('2: 测试 recv()')
+			with conn:
+				conn.sendall('hi from server\n'.encode('utf-8'))
+			print('ok\n')
+
+		elif option == 3:
+			print('3: 测试 recvLineByLine()')
+			with conn:
+				for i in range(3):
+					msg = input('请输入消息: ') + '\n'
+					conn.sendall(msg.encode('utf-8'))
+			print('ok\n')
+
+if __name__ == '__main__':
+	mySocketTest(1)
+	mySocketTest(2)
+	mySocketTest(3)
+*/
